239982b3b4097e3231ab3db920d7c4aeb1660095
G.Provost <grace.g.provost@gmail.com>
2018-01-22 17:41:06 -0800

    start content for beginning page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,27 +3,28 @@ import html from './app.html';
 import './app.css';
 import { removeChildren } from '../dom';
 import Header from './Header';
+import Beginning from '../pages/beginning/Beginning';
 
 const template = new Template(html);
 
 const map = new Map();
-// map.set('#checkout', Checkout);
+map.set('#beginning', Beginning);
 // map.set('#login', Login);
 
 export default class App {
 
-  // constructor() {
-  //   window.onhashchange = () => {
-  //     this.setPage();
-  //   };
-  // }
+  constructor() {
+    window.onhashchange = () => {
+      this.setPage();
+    };
+  }
 
-  // setPage() {
-  //   const Component = map.get(window.location.hash) || Login;
-  //   const component = new Component();
-  //   removeChildren(this.main);
-  //   this.main.appendChild(component.render());
-  // }
+  setPage() {
+    const Component = map.get(window.location.hash) || Beginning;
+    const component = new Component();
+    removeChildren(this.main);
+    this.main.appendChild(component.render());
+  }
 
   render() {
     const dom = template.clone();   
@@ -31,7 +32,7 @@ export default class App {
     dom.querySelector('header').appendChild(new Header().render());
 
     this.main = dom.querySelector('main');
-    // this.setPage();
+    this.setPage();
 
     return dom;
   }
